2090189f2e02313ddc1c7ce852c43e94ed288c9f
[zhangyihao] <[15003125615@163.com]>
2015-08-28 13:14:24 +0800

    fone

---
+++ gittest/gittest/fone.h
@@ -0,0 +1,13 @@
+//
+//  fone.h
+//  gittest
+//
+//  Created by 张益豪 on 15/8/28.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fone : NSObject
+
+@end
---
+++ gittest/gittest/fone.m
@@ -0,0 +1,13 @@
+//
+//  fone.m
+//  gittest
+//
+//  Created by 张益豪 on 15/8/28.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import "fone.h"
+
+@implementation fone
+
+@end
