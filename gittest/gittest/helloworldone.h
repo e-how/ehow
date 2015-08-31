89a59758c2f375300cc7adbf0d8c8ff792ce4fff
[zhangyihao] <[15003125615@163.com]>
2015-08-31 11:06:46 +0800

    test hello world

---
+++ gittest/gittest/helloworldone.h
@@ -0,0 +1,13 @@
+//
+//  helloworldone.h
+//  gittest
+//
+//  Created by 张益豪 on 15/8/31.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface helloworldone : NSObject
+
+@end
---
+++ gittest/gittest/helloworldone.m
@@ -0,0 +1,13 @@
+//
+//  helloworldone.m
+//  gittest
+//
+//  Created by 张益豪 on 15/8/31.
+//  Copyright (c) 2015年 张益豪. All rights reserved.
+//
+
+#import "helloworldone.h"
+
+@implementation helloworldone
+
+@end
