eaec26f884ec78d498b883602b8fb4542750d96c
[zhangyihao] <[15003125615@163.com]>
2015-08-31 11:47:28 +0800

    master

--- gittest/gittest/ViewController.m
+++ gittest/gittest/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"master master");
+    NSLog(@"master master master master");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
