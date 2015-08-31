0162dc7f3bac0cbc87cff11c45cf0294538f41f4
[zhangyihao] <[15003125615@163.com]>
2015-08-31 17:13:04 +0800

    111

--- gittest/gittest/ViewController.m
+++ gittest/gittest/ViewController.m
@@ -19,7 +19,29 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+/*
+ projectfile=`find -d . -name 'project.pbxproj'`
+ projectdir=`echo *.xcodeproj`
+ projectfile="${projectdir}/project.pbxproj"
+ tempfile="${projectdir}/project.pbxproj.out"
+ savefile="${projectdir}/project.pbxproj.mergesave"
+ 
+ cat $projectfile | grep -v "<<<<<<< HEAD" | grep -v "=======" | grep -v "^>>>>>>> " > $tempfile
+ cp $projectfile $savefile
+ mv $tempfile $projectfile
+ 
+ 
+ projectfile=`find -d . -name 'project.pbxproj'`
+ projectdir=`echo *.xcodeproj`
+ projectfile="${projectdir}/project.pbxproj"
+ tempfile="${projectdir}/project.pbxproj.out"
+ savefile="${projectdir}/project.pbxproj.mergesave"
+ 
+ cat $projectfile | grep -v "<<<<<<< HEAD" | grep -v "=======" | grep -v "^>>>>>>> " > $tempfile
+ cp $projectfile $savefile
+ mv $tempfile $projectfile
+ 
+ */
     NSLog(@"master master master master");
     
 
