ab5811d7dd2f47984182565c9bb8e0888450e7b5
zhangyihao <15003125615@163.com>
2015-10-09 13:34:38 +0800

    master log

--- 831test/831test/ViewController.m
+++ 831test/831test/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    
+    NSLog(@"master  add 	");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
